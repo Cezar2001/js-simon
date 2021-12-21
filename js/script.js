f172d4eb0f51052f4fe1e5f3a497fa1f3c087c4f
Cezar <cezarlantosgmail.com>
2021-12-21 16:10:59 +0100

    confronto tra numeri inseriti e random

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function getRandom (min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+let userNumber;
+let sum = ' ';
+const numbers = [];
+const output = document.querySelector('.output'); 
+
+while (numbers.length < 5) {
+    const randomNumber = getRandom(1, 50);
+    if(!numbers.includes(randomNumber)) {
+        numbers.push(randomNumber);
+    }
+} 
+
+setTimeout (function () {
+    alert('Memorizza bene i seguenti numeri');
+    output.innerHTML = numbers; 
+}, 500);
+
+setTimeout (function() {
+    output.innerHTML = '';
+}, 3000);
+
+setTimeout (function() {
+    for (let i = 0; i < 5; i++) {
+        userNumber = parseInt(prompt('Inserire uno dei numeri precedentemente visualizzati!'));
+        sum += userNumber + ',';
+    }
+}, 4000);
+
+setTimeout (function () {
+    if (sum === numbers) {
+        output.innerHTML = 'Complimenti, i numeri da te inseriti sono corretti!!' + sum + numbers; 
+    } else {
+        output.innerHTML = 'Spiacenti, i numeri da te inseriti sono errati!!' + ' ' + 'Numeri precedentemente mostrati: ' + numbers + '!! Numeri da lei inseriti: ' + sum + '!!'; 
+    }
+}, 5000);
+
